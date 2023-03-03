8012a54f39e3e65a62f496eb53c4888c98872daa
Conqcd <411277487@qq.com>
2023-03-03 12:03:46 +0800

    4

--- LocalStiffnessMatrix.cpp
+++ LocalStiffnessMatrix.cpp
@@ -37,35 +37,32 @@ void LocalStiffnessMatrix::create()
     
     // create LSM
     create(gradientMtx);
-
 }
 
 
 void LocalStiffnessMatrix::create(GradientMatrix * gradientMtx)
 {
-        matrix = new PetscScalar();
+    int lsmlen = NODES_PER_ELEMENT * DOF_3D; // 24
+    Matrix LSM(lsmlen,lsmlen), propXgradMtx, tempMtx;
+    
+    // Build Property Matrix
+    PropertyMatrix propMtx(youngsm, poissonsr);
+    
+    
+    // assuming all 8 gradient matrices are available
+    for (int n = 0; n < SAMPLES_PER_ELEMENT; ++n)
+    {// per node n
         
-        int lsmlen = NODES_PER_ELEMENT * DOF_3D; // 24
-        Matrix LSM(lsmlen,lsmlen), propXgradMtx, tempMtx;
+        MatMatMult(*(propMtx.getmat()), *(gradientMtx[n].getmat()), MAT_INITIAL_MATRIX, PETSC_DEFAULT, &propXgradMtx); 
+        MatMatMult(*(gradientMtx[n].getmat_t()), propXgradMtx, MAT_INITIAL_MATRIX, PETSC_DEFAULT, &tempMtx);
         
-        // Build Property Matrix
-        PropertyMatrix propMtx(youngsm, poissonsr);
+        MatAXPY(LSM, gradientMtx[n].getJdet(), tempMtx, DIFFERENT_NONZERO_PATTERN);
         
-        
-        // assuming all 8 gradient matrices are available
-        for (int n = 0; n < SAMPLES_PER_ELEMENT; ++n)
-        {// per node n
-            
-            MatMatMult(*(propMtx.getmat()), *(gradientMtx[n].getmat()), MAT_INITIAL_MATRIX, PETSC_DEFAULT, &propXgradMtx); 
-            MatMatMult(*(gradientMtx[n].getmat_t()), propXgradMtx, MAT_INITIAL_MATRIX, PETSC_DEFAULT, &tempMtx);
-            
-            MatAXPY(LSM, gradientMtx[n].getJdet(), tempMtx, DIFFERENT_NONZERO_PATTERN);
-            
-        }// per integration point i, gradient matrix
+    }// per integration point i, gradient matrix
 
-        //ierr = MatScale(LSM, 0.125 * a * b * c); CHKERRQ(ierr);
-        //ierr = MatScale(LSM, 1.0/1.1); CHKERRQ(ierr);
-        //MatView(LSM, PETSC_VIEWER_STDOUT_WORLD);
-        // Get LSM
-        MatDenseGetArray(LSM, (&matrix));
+    //ierr = MatScale(LSM, 0.125 * a * b * c); CHKERRQ(ierr);
+    //ierr = MatScale(LSM, 1.0/1.1); CHKERRQ(ierr);
+    //MatView(LSM, PETSC_VIEWER_STDOUT_WORLD);
+    // Get LSM
+    MatDenseGetArray(LSM, (&matrix));
 }
