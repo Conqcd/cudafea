e1393f7b96dcbf0b0364126a8fb1f0c73922a255
Conqcd <411277487@qq.com>
2023-03-09 14:30:17 +0800

    11

---
+++ Math/BiCGSTABSolver.cpp
@@ -0,0 +1,25 @@
+#include "BiCGSTABSolver.hpp"
+
+BiCGSTABSolver::BiCGSTABSolver()
+        :iteration_set(1000),iteration_close(0),ResdualNorm(0),tolerance(1e-9)
+{
+}
+
+BiCGSTABSolver::~BiCGSTABSolver()
+{
+}
+
+void BiCGSTABSolver::Solve(const Matrix& A,Vector& x,const Vector& b)
+{
+
+}
+void BiCGSTABSolver::setMaxIteration(idxType iter)
+{
+    iteration_set = iter;
+}
+
+void BiCGSTABSolver::setTolerance(double tol)
+{
+    tolerance = tol;
+}
+
