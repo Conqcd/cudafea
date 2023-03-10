813a82765e3a235708c37958e88005c4eda3f078
Conqcd <411277487@qq.com>
2023-03-10 14:59:08 +0800

    12

---
+++ Math/Solver.cpp
@@ -0,0 +1,30 @@
+#include "Solver.hpp"
+
+extern void PCG(const SymetrixSparseMatrix& A,Vector& x,const Vector& b,double tolerance,int limit,int& iter,double& norm);
+extern void BiCGSTAB(const SymetrixSparseMatrix& A,Vector& x,const Vector& b,double tolerance,int limit,int& iter,double& norm);
+
+Solver::Solver()
+        :iteration_set(1000),iteration_close(0),ResdualNorm(0),tolerance(1e-9)
+{
+}
+
+Solver::~Solver()
+{
+}
+
+void Solver::Solve(const SymetrixSparseMatrix& A,Vector& x,const Vector& b)
+{
+    PCG(A,x,b,tolerance,iteration_set,iteration_close,ResdualNorm);
+    // BiCGSTAB(A,x,b,tolerance,iteration_set,iteration_close,ResdualNorm);
+}
+
+void Solver::setMaxIteration(idxType iter)
+{
+    iteration_set = iter;
+}
+
+void Solver::setTolerance(double tol)
+{
+    tolerance = tol;
+}
+
---
+++ Math/Solver.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Matrix.hpp"
+
+class Solver
+{
+private:
+    int iteration_set,iteration_close;
+    double ResdualNorm;
+    double tolerance;
+public:
+    Solver(/* args */);
+    ~Solver();
+    void Solve(const SymetrixSparseMatrix&,Vector&,const Vector&);
+    void setMaxIteration(idxType);
+    void setTolerance(double);
+
+    inline auto getIteration()const {return iteration_close;}
+    inline auto getResdualNorm()const {return ResdualNorm;}
+};
+
